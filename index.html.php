238a4aa3d83fe128a53f1fc02e3754b0f6d7d4c4
cylskeet <cylskeet@icloud.com>
2017-09-30 11:45:33 -0400

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,643 @@
+<!--
+▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ▓█████▄  ██▓▓█████
+▒██▀ ██▌▓██▒▓█   ▀
+░██   █▌▒██▒▒███
+░▓█▄   ▌░██░▒▓█  ▄
+░▒████▓ ░██░░▒████▒
+ ▒▒▓  ▒ ░▓  ░░ ▒░ ░
+ ░ ▒  ▒  ▒ ░ ░ ░  ░
+ ░ ░  ░  ▒ ░   ░
+   ░     ░     ░  ░
+ ░
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(code credits to xan.ax/badhacker.org)
+-->
+
+
+<html class="gr__shitty_team"><head><script type="text/javascript">document.oncontextmenu = document.body.oncontextmenu = function() {return false;}</script>
+
+
+        <title>
+        shitty.team
+        </title>
+        <meta name="description" content="Fucking Gay HaHa,,">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Fucking Gay HaHa,,">
+        <meta property="og:url" content="http://shitty.team">
+        <meta property="og:description" content="Fucking Gay HaHa,," <meta="">
+        <meta property="og:image:secure_url" content="https://pbs.twimg.com/media/DErPcdmVwAEOzcd.jpg:large">
+        <meta property="og:site_name" content="Fucking Gay HaHa,,">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Fucking Gay HaHa,,">
+        <meta name="twitter:description" content="Fucking Gay HaHa,,">
+        <link rel="stylesheet" href="idk.css">
+        <link rel="icon" type="image/ico" href="http://i.imgur.com/6LU3zJT.png">
+        <meta name="description" content="I EDate Online">
+        <meta property="og:type" content="website">
+        <style id="__web-inspector-hide-shortcut-style__" type="text/css">
+.__web-inspector-hide-shortcut__, .__web-inspector-hide-shortcut__ *, .__web-inspector-hidebefore-shortcut__::before, .__web-inspector-hideafter-shortcut__::after
+{
+    visibility: hidden !important;
+}
+</style>
+<center>
+    <div id="center">
+
+<center>
+<div style="font-size:64px"><font color="#FFFFFF">
+
+    shitty.team</font>
+</div>
+</center>
+<div class="dropmenu">
+	<ul>
+<br><li style="cursor:pointer">blackdust
+		/
+		<ul>
+			<li><div style="font-size:19px"><a href="https://twitter.com/b1ackdust" target="_blank">twitter</a></li>
+			<li><div style="font-size:19px"><a href="https://discord.gg/p8BvwHH" target="_blank">discord</a></li>
+
+
+
+		</ul>
+		</li>
+		<li style="cursor:pointer">jc
+     /
+		<ul>
+			<li><div style="font-size:19px"><a href="https://twitter.com/jc1338" target="_blank">twitter</a></li>
+			<li><div style="font-size:19px"><a href="http://illumni.wtf" target="_blank" class="">site</a></li>
+
+
+      </ul>
+      </li>
+      <li style="cursor:pointer">pat
+
+      <ul>
+        <li><div style="font-size:19px"><a href="https://twitter.com/PatCat187" target="_blank">twitter</a></li>
+
+          <html>
+<head>
+<script type="text/javascript">
+    if (document.addEventListener) { // IE >= 9; other browsers
+        document.addEventListener('contextmenu', function(e) {
+            alert("@jc1338 @b1ackdust"); //here you draw your own menu
+            e.preventDefault();
+        }, false);
+    } else { // IE < 9
+        document.attachEvent('oncontextmenu', function() {
+            alert("@jc1338 @b1ackdust");
+            window.event.returnValue = false;
+        });
+    }
+</script>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<script>
+document.onkeydown = overrideKeyboardEvent;
+document.onkeyup = overrideKeyboardEvent;
+var keyIsDown = {};
+
+function overrideKeyboardEvent(e){
+  switch(e.type){
+    case "keydown":
+      if(!keyIsDown[e.keyCode]){
+        keyIsDown[e.keyCode] = true;
+        // do key down stuff here
+      }
+    break;
+    case "keyup":
+      delete(keyIsDown[e.keyCode]);
+      // do key up stuff here
+    break;
+  }
+  disabledEventPropagation(e);
+  e.preventDefault();
+  return false;
+}
+function disabledEventPropagation(e){
+  if(e){
+    if(e.stopPropagation){
+      e.stopPropagation();
+    } else if(window.event){
+      window.event.cancelBubble = true;
+    }
+  }
+}
+
+</script>
+
+</script>
+
+</html>
+ <script type="text/javascript">
+    var rev = "fwd";
+    function titlebar(val)
+    {
+        var msg  = " shitty.team ";
+        var res = " ";
+        var speed = 100;
+        var pos = val;
+        var le = msg.length;
+        if(rev == "fwd"){
+            if(pos < le){
+            pos = pos+1;
+            scroll = msg.substr(0,pos);
+            document.title = scroll;
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+            else{
+            rev = "bwd";
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+        }
+        else{
+            if(pos > 0){
+            pos = pos-1;
+            var ale = le-pos;
+            scrol = msg.substr(ale,le);
+            document.title = scrol;
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+            else{
+            rev = "fwd";
+            timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+        }
+    }
+
+    titlebar(0);
+    </script>
+
+
+
+
+		</ul>
+		</li></ul></div>
+</div></center><font color="#FFFFFF">
+</font></body></html>
+
+
+                    </div>
+               </div>
+             </script>
+               <audio loop  autoplay>
+             <source src="guap.mp3" type="audio/mp3">
+             </audio>
